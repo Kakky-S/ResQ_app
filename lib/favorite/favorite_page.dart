5f7e502fa7247ce78e14b8d980283e186a454f8e
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-05-07 11:27:02 +0900

    削除機能実装

--- lib/favorite/favorite_page.dart
+++ lib/favorite/favorite_page.dart
@@ -11,28 +11,29 @@ class Favorite extends StatefulWidget{
 }
 
 class _Favorite extends State<Favorite> {
-  List<favorites> favoriteList = [];
-
-  Future<void> page() async{
-    var snapshots = await FirebaseFirestore.instance.collection('mylist').get();
-    var docs = snapshots.docs;
-
-    // タイトルを取得
-    docs.forEach((doc){
-      favoriteList.add(favorites(
-        title: doc.data()['title'],
-        key: doc.data()['key'],
-        //createdTime: doc.data()['created_date']
-      ));
-    });
-    setState(() {});
-
-  }
+  CollectionReference favorite;
+  //List<favorites> favoriteList = [];
+  //
+  // Future<void> page() async{
+  //   var snapshots = await FirebaseFirestore.instance.collection('mylist').get();
+  //   var docs = snapshots.docs;
+  //
+  //   // タイトルを取得
+  //   docs.forEach((doc){
+  //     favoriteList.add(favorites(
+  //       title: doc.data()['title'],
+  //       key: doc.data()['key'],
+  //       //createdTime: doc.data()['created_date']
+  //     ));
+  //   });
+  //   setState(() {});
+  //
+  // }
 
   @override
   void initState(){
     super.initState();
-    page();
+    favorite = FirebaseFirestore.instance.collection('mylist');
   }
 
   // 削除機能
@@ -47,42 +48,54 @@ class _Favorite extends State<Favorite> {
      appBar: AppBar(
        title: Text('お気に入り'),
      ),
-     body: ListView.builder(
-         itemCount: favoriteList.length,
-         itemBuilder: (context, index){
-           return ListTile(
-             title: Text(favoriteList[index].title),
-             trailing: IconButton(
-               icon: Icon(Icons.delete),
-               onPressed: () async {
-                 var result = await showDialog<int>(
-                   context: context,
-                   barrierDismissible: false,
-                   builder: (BuildContext context){
-                     return AlertDialog(
-                       title: Text('本当に削除しますか？'),
-                       content: Text('確認のダイアログです。'),
-                       actions: [
-                         TextButton(
-                             child: Text('OK'),
-                             onPressed: () => Navigator.of(context).pop(0)
-                         ),
-                         TextButton(
-                             child: Text('Cancel'),
-                             onPressed: () => Navigator.of(context).pop(0)
-                         )
-                       ],
-                     );
-                   }
+     body: StreamBuilder<QuerySnapshot>(
+         stream: favorite.snapshots(),
+         builder: (context, snapshot){
+           if(snapshot.connectionState == ConnectionState.waiting) {
+             return CircularProgressIndicator();
+           }
+           return ListView.builder(
+               itemCount: snapshot.data.docs.length,
+               itemBuilder: (context, index){
+                 return ListTile(
+                   title: Text(snapshot.data.docs[index].data()['title']),
+                   trailing: IconButton(
+                     icon: Icon(Icons.delete),
+                     onPressed: () async {
+                       var result = await showDialog<int>(
+                           context: context,
+                           barrierDismissible: false,
+                           builder: (BuildContext context){
+                             return AlertDialog(
+                               title: Text('本当に削除しますか？'),
+                               content: Text('確認のダイアログです。'),
+                               actions: [
+                                 TextButton(
+                                     child: Text('OK'),
+                                     onPressed: () async{
+                                       await deleteFavorite(snapshot.data.docs[index].id);
+                                       Navigator.pop(context);
+                                     }
+                                 ),
+                                 TextButton(
+                                     child: Text('Cancel'),
+                                     onPressed: () => Navigator.pop(context)
+                                 )
+                               ],
+                             );
+                           }
+                       );
+                       //deleteFavorite(favoriteList[index].id);
+                     },
+                   ),
+                   onTap: (){
+                     Navigator.push(context, MaterialPageRoute(builder: (context) => FavoriteSymptom(paramText: snapshot.data.docs[index].data()['key'])));
+                   },
                  );
-                 //deleteFavorite(favoriteList[index].id);
-               },
-             ),
-             onTap: (){
-               Navigator.push(context, MaterialPageRoute(builder: (context) => FavoriteSymptom(paramText: favoriteList[index].key)));
-             },
+               }
            );
          }
+
      ),
    );
 
